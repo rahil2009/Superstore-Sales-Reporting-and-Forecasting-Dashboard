2d44080097ef6af9b2c9becef38beedb432d84bb
rahil2009 <129578049+rahil2009@users.noreply.github.com>
2024-07-12 17:36:32 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.write("""
+    <span style="font-family: Arial, sans-serif; font-size: 20px;">
+         <strong>
+        Superstore Sales Reporting and Forecasting Dashboard
+         </strong>
+    </span>
+""", unsafe_allow_html=True)
+
+st.write("""
+    <span style="font-family: Arial, sans-serif; font-size: 20px;">
+        Our Power BI Super Store Sales Reporting and Forecasting Dashboard provides
+          a comprehensive solution for analyzing sales and profit performance.
+          Executives can make data-driven decisions by examining metrics across different regions, 
+         categories, and segments. The dashboard includes sales analysis, profit analysis,
+          and forecasting capabilities. Stakeholders gain actionable insights to drive business success!
+    </span>
+""", unsafe_allow_html=True)
+
+video_file = open('output.mp4', 'rb')
+
+video_bytes = video_file.read()
+st.video(video_bytes)
